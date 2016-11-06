7601c94b72b60aed498e7640ef6ed8936fc95fe3
Nandini <nandini.s.thakur@gmail.com>
2016-11-05 21:18:33 -0700

    Update and rename JellyfishAutoPushCapShoot.java to JellyfishAutoCapShootRed.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JellyfishAutoCapShootRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JellyfishAutoCapShootRed.java
@@ -1,4 +1,3 @@
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -7,35 +6,15 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
- * This file illustrates the concept of driving a path based on encoder counts.
- * It uses the common Pushbot hardware class to define the drive on the robot.
- * The code is structured as a LinearOpMode
- *
- * The code REQUIRES that you DO have encoders on the wheels,
- *   otherwise you would use: PushbotAutoDriveByTime;
- *
- *  This code ALSO requires that the drive Motors have been configured such that a positive
- *  power command moves them forwards, and causes the encoders to count UP.
- *
- *   The desired path in this example is:
- *   - Drive forward for 48 inches
- *   - Spin right for 12 Inches
- *   - Drive Backwards for 24 inches
- *   - Stop and close the claw.
- *
- *  The code is written using a method called: encoderDrive(speed, leftInches, rightInches, timeoutS)
- *  that performs the actual movement.
- *  This methods assumes that each movement is relative to the last stopping place.
- *  There are other ways to perform encoder based moves, but this method is probably the simplest.
- *  This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ * Push cap ball
+ * Shoot at corner vortex
+ * Park on corner vortex
+ * NEED TO TEST
  */
 
-@Autonomous(name="Jellyfish: Auto PushCapShoot", group="Jellyfish")
+@Autonomous(name="Jellyfish: Auto Cap Shoot Red", group="Jellyfish")
 
-public class JellyfishAutoPushCapShoot extends LinearOpMode {
+public class JellyfishAutoCapShootRed extends LinearOpMode {
 
     /* Declare OpMode members. */
     HardwareJellyfish         robot   = new HardwareJellyfish();   // Use a Pushbot's hardware
@@ -96,6 +75,10 @@ public class JellyfishAutoPushCapShoot extends LinearOpMode {
         encoderTurn(TURN_SPEED, 105, 4.0); // Turn left 90 degress
         sleep(100);
         encoderDrive(DRIVE_SPEED, 0, 50, 4.0);
+        sleep(100);
+        robot.intakeBeltMotor.setPower(-1);
+        sleep(5000);
+        robot.intakeBeltMotor.setPower(0);
 
 
 
