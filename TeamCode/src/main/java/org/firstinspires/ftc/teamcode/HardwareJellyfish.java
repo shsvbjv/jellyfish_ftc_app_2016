1bbc7f71ec30be715765a23dc15282686ef63c6c
nthakur7 <nandini.s.thakur@gmail.com>
2016-11-27 17:26:02 -0800

    base class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareJellyfish.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareJellyfish.java
@@ -88,6 +88,10 @@ public class HardwareJellyfish
 //        flywheelBottomMotor.setPower(0);
 //        conveyerBeltMotor.setPower(0);
 
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         //Set direction of all motors
         
         //I REVERSED THE MOTOR DIRECTIONS
@@ -130,6 +134,7 @@ public class HardwareJellyfish
 
 
 
+
         //flywheelTopMotorRampControl = new RampedMotorControl(flywheelTopMotor, 5.0);
 //        flywheelBottomMotorRampControl = new RampedMotorControl(flywheelBottomMotor, 5.0);
 
@@ -151,7 +156,10 @@ public class HardwareJellyfish
         telemetry.update();
 
 
+
     }
+
+
 
     /***
      *
